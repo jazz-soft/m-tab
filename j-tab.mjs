44a03e71abe8bd53e98f93e9b176543fe419a0ae
Sema <jazz-dev@jazz-soft.net>
2024-06-09 20:42:32 -0400

    eork in progress

--- j-tab.mjs
+++ j-tab.mjs
@@ -1,9 +1,14 @@
 import {EditorView, basicSetup} from "codemirror";
-import {JZZ} from "jzz";
-import {KBD} from "jzz-input-kbd";
-import {TINY} from "jzz-synth-tiny";
+import JZZ from "jzz";
+import KBD from "jzz-input-kbd";
+import TINY from "jzz-synth-tiny";
 KBD(JZZ);
 TINY(JZZ);
+
+JZZ.synth.Tiny.register('Web Audio');
+var piano = JZZ.input.Kbd({ at: "piano" });
+var out = JZZ().openMidiOut();
+piano.connect(out);
 
 let editor = new EditorView({
   extensions: [basicSetup],
